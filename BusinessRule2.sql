bb27d7dffdc6634db22342f477dfc2f5a0e968ce
WilliamGlory <williamgloryh@gmail.com>
2024-05-14 00:27:03 +0700

    feat: add Business Rule 2

---
+++ BusinessRule2.sql
@@ -0,0 +1,69 @@
+CREATE TABLE pengguna_aktif (
+    email_pengguna VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    nama_depan VARCHAR(255) NOT NULL,
+    nama_belakang VARCHAR(255) NOT NULL,
+    tanggal_lahir DATE NOT NULL,
+    level INT NOT NULL,
+    PRIMARY KEY (`email_pengguna`)
+);
+
+INSERT INTO pengguna_aktif
+SELECT p.*
+FROM pengguna p
+JOIN aplikasi_pengguna ap ON p.email_pengguna = ap.email_pengguna
+GROUP BY p.email_pengguna;
+
+CREATE TABLE pengguna_pasif (
+    email_pengguna VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    nama_depan VARCHAR(255) NOT NULL,
+    nama_belakang VARCHAR(255) NOT NULL,
+    tanggal_lahir DATE NOT NULL,
+    level INT NOT NULL,
+    PRIMARY KEY (`email_pengguna`)
+);
+
+INSERT INTO pengguna_pasif
+SELECT p.*
+FROM pengguna p
+LEFT JOIN aplikasi_pengguna ap ON p.email_pengguna = ap.email_pengguna
+WHERE ap.email_pengguna IS NULL;
+
+ALTER TABLE aplikasi_pengguna DROP FOREIGN KEY email_pengguna;
+ALTER TABLE aplikasi_pengguna
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_aktif` (`email_pengguna`);
+
+ALTER TABLE pengikut_developer DROP FOREIGN KEY email_pengguna;
+ALTER TABLE pengikut_developer
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_aktif` (`email_pengguna`),
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_pasif` (`email_pengguna`);
+
+ALTER TABLE forum DROP FOREIGN KEY email_pengguna;
+ALTER TABLE forum
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_aktif` (`email_pengguna`),
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_pasif` (`email_pengguna`);
+
+ALTER TABLE post DROP FOREIGN KEY email_pengguna;
+ALTER TABLE post
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_aktif` (`email_pengguna`),
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_pasif` (`email_pengguna`);
+
+ALTER TABLE vote_forum DROP FOREIGN KEY email_pengguna;
+ALTER TABLE vote_forum
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_aktif` (`email_pengguna`),
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_pasif` (`email_pengguna`);
+
+ALTER TABLE status_pertemanan DROP FOREIGN KEY email_pengguna;
+ALTER TABLE status_pertemanan
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_aktif` (`email_pengguna`),
+ADD FOREIGN KEY (`email_pengguna`) REFERENCES `pengguna_pasif` (`email_pengguna`);
+
+ALTER TABLE status_pertemanan DROP FOREIGN KEY email_teman;
+ALTER TABLE status_pertemanan
+ADD FOREIGN KEY (`email_teman`) REFERENCES `pengguna_aktif` (`email_pengguna`),
+ADD FOREIGN KEY (`email_teman`) REFERENCES `pengguna_pasif` (`email_pengguna`);
+
+DROP TABLE pengguna
