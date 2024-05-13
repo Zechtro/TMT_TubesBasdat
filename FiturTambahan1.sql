0379900fd95b55aecf9fef71ecf1d0707909edc6
WilliamGlory <williamgloryh@gmail.com>
2024-05-13 17:30:21 +0700

    fix: fix fitur tambahan 1

--- FiturTambahan1.sql
+++ FiturTambahan1.sql
@@ -1,26 +1,12 @@
 -- 1. Email Validation
 -- Pastikan bahwa seluruh data email yang ada pada database merupakan email yang benar. Asumsikan format email yang benar adalah “<...>@<...>.com”. Bagian “<...>” dapat diisi oleh potongan String apapun. Buatlah check constraint untuk query insert yang mungkin terjadi di masa yang akan datang.
-DELIMITER $$
 
-CREATE TRIGGER `validasi_email_developer`
-BEFORE INSERT
-  ON developer FOR EACH ROW
-BEGIN
-    IF NEW.email_developer NOT LIKE '%@%.com' THEN 
-      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Email developer tidak valid';
-    END IF;
-END
-$$
-DELIMITER ;
+-- email_developer
+ALTER TABLE developer
+ADD CONSTRAINT chk_email_developer
+CHECK (email_developer LIKE '%@%.com');
 
-DELIMITER $$
-CREATE TRIGGER `validasi_email_pengguna`
-BEFORE INSERT ON pengguna
-FOR EACH ROW
-BEGIN
-    IF NEW.email_pengguna NOT LIKE '%@%.com' THEN 
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Email pengguna tidak valid';
-    END IF;
-END
-$$
-DELIMITER ;
+-- email_pengguna
+ALTER TABLE pengguna
+ADD CONSTRAINT chk_email_pengguna
+CHECK (email_pengguna LIKE '%@%.com');
