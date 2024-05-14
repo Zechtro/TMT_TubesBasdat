881c2eddc9606ffb2f121f4976acd397c4e1cf13
WilliamGlory <williamgloryh@gmail.com>
2024-05-14 13:59:05 +0700

    feat: change TypeConstraint

--- TypeConstraint.sql
+++ TypeConstraint.sql
@@ -2,9 +2,17 @@
 -- Persoalan rating harus dipastikan (5 >= r >= 0)
 
 -- tipe harga
-TYPE HRG POSSREP (INT)
-CONSTRAINT tipe_harga (HRG) >= 0;
+-- TYPE HRG POSSREP (INT)
+-- CONSTRAINT tipe_harga (HRG) >= 0;
+
+ALTER TABLE aplikasi
+ADD CONSTRAINT chk_harga
+CHECK (harga >= 0);
 
 -- tipe rating
-TYPE RTG POSSREP (INT)
-CONSTRAINT tipe_rating ((RTG) >= 0 AND (RTG) <= 5);
+-- TYPE RTG POSSREP (INT)
+-- CONSTRAINT tipe_rating ((RTG) >= 0 AND (RTG) <= 5);
+
+ALTER TABLE aplikasi_pengguna
+ADD CONSTRAINT chk_user_rating
+CHECK (user_rating >= 0 AND user_rating <= 5);
