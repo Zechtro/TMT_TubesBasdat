0d8a7a9f4e408c8f02edf145a782279a81f27086
Yusufarsan <sandiyusufardian@gmail.com>
2024-05-17 19:57:29 +0700

    Add code implementasi SQL in Databse Constraint

--- DatabaseConstraint.sql
+++ DatabaseConstraint.sql
@@ -2,8 +2,29 @@
 CONSTRAINT THN_AWARDS
     IS_EMPTY ((awards NATURAL JOIN aplikasi)
         WHERE (awards.tahun_award >= YEAR(aplikasi.tanggal_peluncuran)));
+-- Implementasi SQL
+CREATE TRIGGER check_awards_trigger
+BEFORE INSERT OR UPDATE ON awards
+FOR EACH ROW
+BEGIN
+    IF NEW.tahun_award <= (SELECT YEAR(tanggal_peluncuran) FROM aplikasi NATURAL JOIN awards WHERE NEW.awards.judul_vidgame = aplikasi.judul_aplikasi) THEN
+        RAISE EXCEPTION 'tahun_award must be later than the YEAR(tanggal_peluncuran) of the video game';
+    END IF;
+    RETURN NEW;
+END;
+
 
 -- Constraint untuk memastikan bahwa tanggal peluncuran dlc harus setelah tanggal peluncuran aplikasi
 CONSTRAINT TGL_DLC
     IS_EMPTY ((dlc NATURAL JOIN aplikasi)
         WHERE (dlc.tanggal_peluncuran_dlc < aplikasi.tanggal_peluncuran));
+-- Implementasi SQL
+CREATE TRIGGER check_dlc_launch_date_trigger
+BEFORE INSERT OR UPDATE ON awards
+FOR EACH ROW
+BEGIN
+    IF NEW.tanggal_peluncuran_dlc <= (SELECT tanggal_peluncuran FROM aplikasi NATURAL JOIN dlc WHERE NEW.dlc.judul_vidgame = aplikasi.judul_aplikasi) THEN
+        RAISE EXCEPTION 'tanggal_peluncuran_dlc must be later than the tanggal_peluncuran of the video game';
+    END IF;
+    RETURN NEW;
+END;
