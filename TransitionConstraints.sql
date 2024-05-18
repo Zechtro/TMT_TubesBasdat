5a846b59111a4b63ff4eb0e53b8c45f2eeb55f90
Yusufarsan <sandiyusufardian@gmail.com>
2024-05-18 16:46:34 +0700

    Added implementasi sintaks PPT

--- TransitionConstraints.sql
+++ TransitionConstraints.sql
@@ -1,3 +1,9 @@
+-- Implementasi Sintaks PPT (ini emang jadi ga jelas sintaks nya gegara ngikutin PPT)
+CONSTRAINT prevent_status_friends_to_pending IS_EMPTY
+( ( ( status_pertemanan' (email_pengguna, email_teman, status) RENAME status AS status')
+    JOIN status_pertemanan (email_pengguna, email_teman, status) )
+    WHERE status' = 'FRIENDS' AND status = 'PENDING' );
+
 DELIMITER $$
 CREATE TRIGGER prevent_status_friends_to_pending
 BEFORE UPDATE ON status_pertemanan
@@ -9,6 +15,13 @@ BEGIN
     END IF;
 END $$
 DELIMITER ;
+
+-- Implementasi Sintaks PPT (ini emang jadi ga jelas sintaks nya gegara ngikutin PPT)
+CONSTRAINT prevent_status_block_to_pending IS_EMPTY
+( ( ( status_pertemanan' (email_pengguna, email_teman, status) RENAME status AS status')
+    JOIN status_pertemanan (email_pengguna, email_teman, status) )
+    WHERE status' = 'BLOCK' AND status = 'PENDING' );
+
 -- asumsi block tidak menghapus teman, jika di-unblock maka status akan menjadi FRIENDS
 DELIMITER $$
 CREATE TRIGGER prevent_status_block_to_pending
